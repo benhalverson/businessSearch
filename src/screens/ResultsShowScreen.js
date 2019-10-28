3f0f20ae3a392fdab4c2d2e701832cfec88954f1
Ben Halverson <benhalverson33@gmail.com>
2019-10-27 23:48:32 -0700

    feat(details screen): able to show name and images for each company

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+	const [ result, setResult ] = useState(null);
+	const id = navigation.getParam('id');
+	const getResult = async (id) => {
+		const response = await yelp.get(`/${id}`);
+		setResult(response.data);
+	};
 
-  return(
-    <View>
-      <Text>Results Show Screen</Text>
-    </View>
-  )
-}
+	useEffect(() => {
+		getResult(id);
+  }, []);
+  
+  if (!result) {
+    return null;
+  }
+	return (
+		<View>
+			<Text style={styles.title}>{result.name}</Text>
+      <FlatList 
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={( {item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />
+        }}
+      />
 
-const styles = StyleSheet.create({});
+      
+		</View>
+	);
+};
+
+
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+    marginBottom: 10,
+    marginLeft: 15,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 10,
+    alignSelf: 'center'
+  }
+});
 
 export default ResultsShowScreen;
