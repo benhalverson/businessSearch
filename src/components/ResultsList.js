82d54f748521a04b076d2a7da4dc6a4844a615ef
Ben Halverson <benhalverson33@gmail.com>
2019-10-27 23:04:10 -0700

    feat(details screen): added details screen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>{title}</Text>
-      <FlatList 
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={results}
-        keyExtractor={(result) => result.id}
-        renderItem={({item}) => {
-          return <ResultsDetail result={item} />
-        }}
-      />
+			<FlatList
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				data={results}
+				keyExtractor={(result) => result.id}
+				renderItem={({ item }) => {
+					return (
+						<TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+							<ResultsDetail result={item} />
+						</TouchableOpacity>
+					);
+				}}
+			/>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginLeft: 15, 
-    marginBottom: 5
-  },
-  container: {
-    marginBottom: 10
-  }
+	title: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		marginLeft: 15,
+		marginBottom: 5
+	},
+	container: {
+		marginBottom: 10
+	}
 });
 
 export default ResultsList;
---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsShowScreen = () => {
+
+  return(
+    <View>
+      <Text>Results Show Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
